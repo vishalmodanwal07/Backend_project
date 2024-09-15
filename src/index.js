5cee50aed61eaabd62690bfd1b5eaed544566588
vishal modanwal <vk0764394@gmail.com>
2024-09-15 22:00:13 +0530

    mongodb connection done

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv"; 
+import connectDB from "./db/index.js";
+
+
+dotenv.config({path:'./env'});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+
+
+const app = express();
+
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+        app.on("error" , (error) =>{
+            console.log("error:");
+        })
+
+        app.listen(process.env.PORT , ()=>{
+            console.log(`you are listeaning at port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR:");
+        
+    }
+}
+)();*/
