563e553d0c11ef14d54efb4d3493e6b04fffbfba
vishal modanwal <vk0764394@gmail.com>
2024-10-22 01:26:32 +0530

    correction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
 const generateAccessAndRefreshToken = async(userId)=>{
    try {
